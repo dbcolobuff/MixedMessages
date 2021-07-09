858bd757d21c12f80a1578b4d1e7021a7ce48a2d
Daniel Bonnecaze <danielbonnecaze@gmail.com>
2021-07-09 11:20:02 -0600

    Add driver info and races

---
+++ MixedMessages.js
@@ -0,0 +1,104 @@
+const grandPrixs = ['Australian','Spanish', 'Monaco', 'French', 'Canadian', 'Austrian', 'British', 'German', 'Dutch', 'Belgian', 'Italian', 'Japanese', 'American', 'Brazilian'];
+const drivers = [
+    {
+        name: 'Max Verstappen',
+        team: 'Red Bull Racing',
+        nationality: 'Dutch',
+    },
+    {
+        name: 'Lewis Hamilton',
+        team: 'Mercedes',
+        nationality: 'British',
+    },
+    {
+        name: 'Sergio Perez',
+        team: 'Red Bull Racing',
+        nationality: 'Mexican',
+    },
+    {
+        name: 'Lando Norris',
+        team: 'McLaren',
+        nationality: 'British',
+    },
+    {
+        name: 'Valterri Bottas',
+        team: 'Mercedes',
+        nationality: 'Finnish',
+    },
+    {
+        name: 'Charles Leclerc',
+        team: 'Ferrari',
+        nationality: 'Monaco',
+    },
+    {
+        name: 'Carlos Sainz',
+        team: 'Ferrari',
+        nationality: 'Spanish',
+    },
+    {
+        name: 'Daniel Ricciardo',
+        team: 'McLaren',
+        nationality: 'Australian',
+    },
+    {
+        name: 'Pierre Gasly',
+        team: 'AlphaTauri',
+        nationality: 'French',
+    },
+    {
+        name: 'Sebastian Vettel',
+        team: 'Aston Martin',
+        nationality: 'German',
+    },
+    {
+        name: 'Fernando Alonso',
+        team: 'Alpine',
+        nationality: 'Spanish',
+    },
+    {
+        name: 'Lance Stroll',
+        team: 'Aston Martin',
+        nationality: 'Canadian',
+    },
+    {
+        name: 'Esteban Ocon',
+        team: 'Alpine',
+        nationality: 'French',
+    },
+    {
+        name: 'Yuki Tsunoda',
+        team: 'AlphaTauri',
+        nationality: 'Japanese',
+    },
+    {
+        name: 'Kimi Raikkonen',
+        team: 'Alfa Romeo',
+        nationality: 'Finnish',
+    },
+    {
+        name: 'Antonio Giovinazzi',
+        team: 'Alfa Romeo',
+        nationality: 'Italian',
+    },
+    {
+        name: 'George Russell',
+        team: 'Williams',
+        nationality: 'British',
+    },
+    {
+        name: 'Mick Schumacher',
+        team: 'Haas',
+        nationality: 'German',
+    },
+    {
+        name: 'Nikita Mazepin',
+        team: 'Haas',
+        nationality: 'Dutch',
+    },
+    {
+        name: 'Nicholas Latifi',
+        team: 'Williams',
+        nationality: 'Canadian',
+    },
+
+];
